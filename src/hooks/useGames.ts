07fc553b686ea520b4a3dad99f7f929ffc79be11
sadibaba2000 <77102348+sadibaba2000@users.noreply.github.com>
2023-11-02 22:17:26 +0100

    Refactoring- fetch games with React Query

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,34 +1,37 @@
+import { useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
-import useData from "./useData";
-import { Genre } from "./useGenres";
+import apiClient from "../services/api-client";
+import { FetchResponse } from "../services/api-client";
 
 export interface Platform {
-  id: number;
-  name: string;
-  slug: string;
+    id: number;
+    name: string;
+    slug: string;
 }
 
 export interface Game {
-  id: number;
-  name: string;
-  background_image: string;
-  parent_platforms: { platform: Platform }[];
-  metacritic: number;
-  rating_top: number;
+    id: number;
+    name: string;
+    background_image: string;
+    parent_platforms: { platform: Platform }[];
+    metacritic: number;
+    rating_top: number;
 }
 
 const useGames = (gameQuery: GameQuery) =>
-  useData<Game>(
-    "/games",
-    {
-      params: {
-        genres: gameQuery.genre?.id,
-        platforms: gameQuery.platform?.id,
-        ordering: gameQuery.sortOrder,
-        search: gameQuery.searchText
-      },
-    },
-    [gameQuery]
-  );
+    useQuery<FetchResponse<Game>, Error>({
+        queryKey: ["games", gameQuery], // den siste er for å lage dependencies,
+        queryFn: () =>
+            apiClient
+                .get<FetchResponse<Game>>("/games", {
+                    params: {
+                        genres: gameQuery.genre?.id,
+                        parent_platforms: gameQuery.platform?.id,
+                        ordering: gameQuery.sortOrder,
+                        search: gameQuery.searchText, //done by java developer -sarim
+                    },
+                })
+                .then((res) => res.data),
+    });
 
 export default useGames;
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,13 @@
 import axios from "axios";
 
+export interface FetchResponse<T> {
+    count: number;
+    results: T[];
+}
+
 export default axios.create({
-  baseURL: "https://api.rawg.io/api",
-  params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
-  },
+    baseURL: "https://api.rawg.io/api",
+    params: {
+        key: "7a45a085d8734d138f4cdb72655a7926",
+    },
 });
